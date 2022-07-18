18ab4ed34cba694404a6f9f7bd42d927fd8a534e
Herizoran <herizo.ran@gmail.com>
2022-07-18 18:07:59 +0300

    updated queries indentation

--- queries/query_id_trajet_to_mahajanga.sql
+++ queries/query_id_trajet_to_mahajanga.sql
@@ -1,1 +1,7 @@
-select * from trajet where id_ville_arrivee = (select id_ville from ville where nom_ville = 'Mahajanga');
+select * 
+from trajet 
+where 
+	id_ville_arrivee = (select id_ville 
+						from ville 
+						where nom_ville = 'Mahajanga'
+						);
--- queries/query_voyage_from_mahajanga.sql
+++ queries/query_voyage_from_mahajanga.sql
@@ -1,1 +1,9 @@
-select * from voyage where id_trajet=(select id_trajet from trajet where id_ville_depart = (select id_ville from ville where nom_ville = 'Mahajanga'));
+select * 
+from voyage 
+where 
+	id_trajet=(select id_trajet 
+				from trajet 
+				where id_ville_depart = (select id_ville 
+										from ville 
+										where nom_ville = 'Mahajanga')
+										);
